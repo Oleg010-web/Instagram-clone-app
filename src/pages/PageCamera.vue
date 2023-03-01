3cdc1cc1e6ced2a68aeb2d554112504b6e40fe44
Oleg010-web <nesteroff_oleg@mail.ru>
2023-03-01 16:56:33 +0700

    Take a photo by users camera and show it on camera page

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -2,14 +2,21 @@
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
       <video 
+        v-show="!imageCaptured.captured"
         ref="video"
         class="full-width"
         autoplay
         playsinline
       />
+      <canvas 
+        v-show="imageCaptured.captured"
+        ref="canvas"
+        class="full-width"
+        height="200"
+      />
     </div>
     <div class="text-center q-pa-md">
-      <q-btn round color="grey-10" size="lg" icon="eva-camera" />
+      <q-btn @click="captureImage" round color="grey-10" size="lg" icon="eva-camera" />
       <div class="row justify-center q-ma-md">
         <q-input
           class="col col-sm-6"
@@ -40,6 +47,7 @@
 <script setup>
 import { uid } from 'quasar'
 import { reactive, onMounted, ref } from 'vue'
+import * as locales  from 'md-gum-polyfill'
 
 const post = reactive({
   id: uid(),
@@ -49,8 +57,21 @@ const post = reactive({
   date: Date.now()
 })
 
-let video = ref()
+const imageCaptured = reactive ({
+  captured: false
+})
 
+let video = ref()
+let canvas = ref()
+
+const captureImage = () => {
+  canvas.value.width = video.value.getBoundingClientRect().width
+  canvas.value.height = video.value.getBoundingClientRect().height
+  let context = canvas.value.getContext('2d')
+  context.drawImage(video.value, 0, 0, canvas.value.width, canvas.value.height)
+  imageCaptured.captured = true
+
+}
 const initCamera = () => {
   navigator.mediaDevices.getUserMedia({
     video: true
