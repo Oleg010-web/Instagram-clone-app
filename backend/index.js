db6ad0f79467dcc15fa21a0389fe227390ee869f
Oleg010-web <nesteroff_oleg@mail.ru>
2023-03-10 14:38:29 +0700

    Little refactoring written before code

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,30 @@
 // dependencies
 
-    const express = require('express')
+const express = require('express')
 
-// config-express    
+// config-express
 
-    const app = express()
-    const port = 3000
+const app = express()
+const port = 3000
 
 // endpoint - posts
 
-    app.get('/posts', (request, response) => {
-    let posts = [
-        {
-            caption: 'Golden Gate Bridge',
-            location: 'San Francisco'
-        },
-        {
-            caption: 'Drottningholm',
-            location: 'Stockholm'
-        }
-    ]
-    response.send(posts)
-    })
+app.get('/posts', (request, response) => {
+  let posts = [
+    {
+      caption: 'Golden Gate Bridge',
+      location: 'San Francisco'
+    },
+    {
+      caption: 'Drottningholm',
+      location: 'Stockholm'
+    }
+  ]
+  response.send(posts)
+})
 
 // listen
 
-    app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-    })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
