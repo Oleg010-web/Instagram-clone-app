39feef4277ed6b9b51023a12e055c77f26ea2563
Oleg010-web <nesteroff_oleg@mail.ru>
2023-03-11 19:20:59 +0700

    Backend settings, connection between our server and firebase, get data from firebase

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,37 @@
 // dependencies
 
 const express = require('express')
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
 
 // config-express
 
 const app = express()
 const port = 3000
 
+// config - firebase
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
 // endpoint - posts
 
 app.get('/posts', (request, response) => {
-  let posts = [
-    {
-      caption: 'Golden Gate Bridge',
-      location: 'San Francisco'
-    },
-    {
-      caption: 'Drottningholm',
-      location: 'Stockholm'
-    }
-  ]
-  response.send(posts)
+  let posts = [];
+  db.collection('posts').get().then(snapshot => {
+      snapshot.forEach((doc) => {
+        posts.push(doc.data())
+    });
+   response.send(posts) 
+  })
+    
+  
 })
 
 // listen
