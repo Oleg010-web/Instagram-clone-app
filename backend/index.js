98dc4c7b61914a86a4143528b51ba1f13806fe15
Oleg010-web <nesteroff_oleg@mail.ru>
2023-03-17 21:49:32 +0700

    Working on backend, now we can send data to firebase db

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,8 @@ const {
   Timestamp,
   FieldValue
 } = require('firebase-admin/firestore')
+const { getStorage } = require('firebase-admin/storage');
+const busboy = require('busboy');
 
 // config-express
 
@@ -22,10 +24,12 @@ const port = 3000
 const serviceAccount = require('./serviceAccountKey.json')
 
 initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
+  storageBucket: 'gs://social-media-on-quasar.appspot.com'
 })
 
-const db = getFirestore()
+const db = getFirestore();
+const bucket = getStorage().bucket();
 
 // endpoint - posts
 
@@ -48,7 +52,37 @@ app.get('/posts', (request, response) => {
 app.post('/createPost', (request, response) => {
   response.set('Access-Control-Allow-Origin', '*')
  
-  response.send(request.headers)
+  const bb = busboy({ headers: request.headers });
+  let fields = {}
+  bb.on('file', (name, file, info) => {
+    const { filename, encoding, mimeType } = info;
+    console.log(
+      `File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
+      filename,
+      encoding,
+      mimeType
+    );
+    file.on('data', (data) => {
+      console.log(`File [${name}] got ${data.length} bytes`);
+    }).on('close', () => {
+      console.log(`File [${name}] done`);
+    });
+  });
+  bb.on('field', (name, val, info) => {
+    fields[name] = val
+  });
+  bb.on('close', () => {
+    db.collection('posts').doc(fields.id).set({
+      id: fields.id,
+      caption: fields.caption,
+      location: fields.location,
+      date: parseInt(fields.date),
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/social-media-on-quasar.appspot.com/o/Drottningholm.jpeg?alt=media&token=477b97a4-bd3b-47aa-9ced-9bc1cba25261'
+    });
+    // response.writeHead(303, { Connection: 'close', Location: '/' });
+    response.send('Done!')
+  });
+  request.pipe(bb);
 })
 
 
